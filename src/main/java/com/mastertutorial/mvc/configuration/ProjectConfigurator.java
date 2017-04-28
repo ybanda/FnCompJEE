b70bef7c5fcf9857113602aea575bc5e2eea82ee
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-04-28 01:08:56 -0400

    syncing Old Files from existing project

---
+++ src/main/java/com/mastertutorial/mvc/configuration/ProjectConfigurator.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.mastertutorial.mvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Yashwanth
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.mastertutorial")
+public class ProjectConfigurator extends WebMvcConfigurerAdapter{
+	public ProjectConfigurator() {
+
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setViewClass(JstlView.class);
+		internalResourceViewResolver.setSuffix(".jsp");
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		registry.viewResolver(internalResourceViewResolver);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/client/**").addResourceLocations("/client/**");
+	}
+}
